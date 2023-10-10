211daa5a95d83897618a10061ef3f53b01f834c6
Sarah <143218168+Sarah-S-R@users.noreply.github.com>
2023-10-10 19:43:17 -0230

    added js file

---
+++ main.js
@@ -0,0 +1,9 @@
+ // Get a reference to the member login button by its id
+const memberLoginButton = document.getElementById("member-login-button"); 
+
+  // Define the function to run when the button is clicked
+  function handleMemberLoginClick( ) {
+    alert("Member login clicked. Open account.")
+}
+ memberLoginButton.addEventListener("click", handleMemberLoginClick);
+
